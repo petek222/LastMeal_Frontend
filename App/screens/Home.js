69c1abeafa2f383b654be310cdf6e46634aab132
Young-Rae Peter Kim <youngraek555@gmail.com>
2021-02-21 17:35:36 -0600

    login

---
+++ App/screens/Home.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { StatusBar, View, StyleSheet, Dimensions, Text, ScrollView, TouchableOpacity } from 'react-native';
+
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const screen = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+
+
+})
+
+export default ({navigation}) => {
+    return <View>
+        <Text>asdf</Text>
+    </View>
+}
