42f8fc66ad9046751742eb878263a9fce051342e
petek222 <peter.e.koncelik@vanderbilt.edu>
2021-03-05 02:04:32 -0600

    added navbar

--- App/config/Navigation.js
+++ App/config/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Home from '../screens/Home';
 import Login from '../screens/Login';
@@ -10,21 +11,27 @@ import Pantry from '../screens/Pantry';
 import Recipes from '../screens/Recipes';
 import Options from '../screens/Options';
 
+// custom_styling = {
+//     fontSize: 20
+// }
+
+const Tab = createBottomTabNavigator();
+
 const MainStack = createStackNavigator();
 const MainStackScreen = () => (
-    <MainStack.Navigator
+    <Tab.Navigator
         // headerMode='none'
         // initialRouteName="Profile"
         >
-        <MainStack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-        <MainStack.Screen name="Signup" component={Signup} options={{ headerShown: false }} />
-        <MainStack.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
-        <MainStack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-        <MainStack.Screen name="Pantry" component={Pantry} options={{ headerShown: false }} />
-        <MainStack.Screen name="Recipes" component={Recipes} options={{ headerShown: false }} />
-        <MainStack.Screen name="Options" component={Options} />
+        <Tab.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        <Tab.Screen name="Signup" component={Signup} options={{ headerShown: false }} />
+        <Tab.Screen name="Home" component={Home} options={{ headerShown: false }} />
+        <Tab.Screen name="Pantry" component={Pantry} options={{ headerShown: false }} />
+        <Tab.Screen name="Recipes" component={Recipes} options={{ headerShown: false }} />
+        <Tab.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
+        <Tab.Screen name="Options" component={Options} />
 
-    </MainStack.Navigator>
+    </Tab.Navigator>
 );
 
 export default () => (
