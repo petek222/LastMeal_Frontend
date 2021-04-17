aefae98a33a179b7920e9c65599ab7b5604a5ff7
petek222 <peter.e.koncelik@vanderbilt.edu>
2021-04-17 11:36:46 -0500

    removing junk from index.js

--- App/index.js
+++ App/index.js
@@ -37,28 +37,6 @@ export default function App() {
   }, []);
 
   return (<Navigation />);
-
-//   return (
-//     <View
-//       style={{
-//         flex: 1,
-//         alignItems: 'center',
-//         justifyContent: 'space-around',
-//       }}>
-//       <Text>Your expo push token: {expoPushToken}</Text>
-//       <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-//         <Text>Title: {notification && notification.request.content.title} </Text>
-//         <Text>Body: {notification && notification.request.content.body}</Text>
-//         <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
-//       </View>
-//       <Button
-//         title="Press to schedule a notification"
-//         onPress={async () => {
-        //   await schedulePushNotification();
-//         }}
-//       />
-//     </View>
-//   );
 }
 
 async function schedulePushNotification() {
@@ -102,30 +80,3 @@ async function registerForPushNotificationsAsync() {
 
   return token;
 }
-
-// import React, { useState, useEffect } from 'react';
-// import Navigation from './config/Navigation';
-
-// import * as Notifications from 'expo-notifications'
-// import * as Permissions from 'expo-permissions';
-
-// const registerForPushNotifications = async () => { 
-//     try {
-//        const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-//        if (!permission.granted) return;
-//        const token = await Notifications.getExpoPushTokenAsync();
-//     console.log("PUSH NOTIFICATION TOKEN TEST")
-//     console.log(token);
-//     } catch (error) {
-//       console.log('Error getting a token', error);
-//     }
-//   }
-
-// export default () => {
-
-//     useEffect(() => {
-//         registerForPushNotifications();
-//       }, [])
-
-//     return (<Navigation />);
-// } 
