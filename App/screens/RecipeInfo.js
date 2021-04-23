f880ca1a47d46278a4430e19c8abac0cac70f50d
petek222 <peter.e.koncelik@vanderbilt.edu>
2021-04-23 02:23:04 -0500

    basic recipeInfo connection

--- App/screens/RecipeInfo.js
+++ App/screens/RecipeInfo.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
-import { Dimensions, StyleSheet, SafeAreaView, ScrollView, Text, View } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { Dimensions, StyleSheet, SafeAreaView, ScrollView, Text, View, StatusBar } from "react-native";
 import Constants from 'expo-constants';
 import { Thumbnail } from 'native-base';
 import { useIsFocused } from "@react-navigation/native";
+import Loader from '../config/Loader'
 
 // import moment from 'moment';
 import api from '../api/api';
@@ -81,7 +82,9 @@ const styles = StyleSheet.create({
 export default ({route, navigation}) => {
 
     // replace require with prop json data passed into this component
-    const recipeData = require('../assets/recipeData.json');
+    // const recipeData = require('../assets/recipeData.json');
+
+    const [recipeData, setRecipeData] = useState(null)
 
     console.log("CARD GEN ID TEST");
     console.log(route.params)
@@ -96,13 +99,13 @@ export default ({route, navigation}) => {
 
             console.log("Making Spoonacular API Request for Recipe Info")
 
-            let recipeData = await getRecipeInfo(route.params);
+            let recipeData = await getRecipeInfo(route.params.recipeID);
     
             // Here is where we want to work on the recipe data sent from the API to build our cards
             console.log("Returned Recipe")
             console.log(recipeData)
                 
-            // await setRecipes(recipeList)
+            await setRecipeData(recipeData)
 
             // Otherwise, set the recipe list again (?)
         }
@@ -128,67 +131,78 @@ export default ({route, navigation}) => {
         }
     }
 
+    if (recipeData != null) {
+        console.log("Generation")
+        let recipeDataParsed = recipeData.data.recipe_data.recipe_info
+        return (
+            <SafeAreaView style={styles.safeAreaView}>
+                <ScrollView contentContainerStyle={styles.scrollViewContent}>
+    
+                    <View style={styles.headerContainer}>
+                        <View style={styles.nameContainer}>
+                            <Text style={styles.recipeNameText}>{recipeDataParsed.title}</Text>
+                        </View>
+                        <View>
+                            <Thumbnail style={styles.thumbnail} source={{uri: recipeDataParsed.image}}/>
+                        </View>
+                    </View>
+    
+                    <View style={styles.bodyContainer}>
+                        <View style={styles.infoContainer}>
+                            <Text style={styles.infoHeader}>Description</Text>
+                            <View style={styles.infoContent}>
+                                <Text style={styles.contentItem}>{recipeDataParsed.summary.replace(/(<([^>]+)>)/gi, "")}</Text>
+                            </View>
+                        </View>
+                        <View style={styles.infoContainer}>
+                            <Text style={styles.infoHeader}>Time</Text>
+                            <View style={styles.infoContent}>
+                                <Text style={styles.contentItem}>Preparation: {recipeDataParsed.preparationMinutes} mins</Text>
+                                <Text style={styles.contentItem}>Cooking: {recipeDataParsed.cookingMinutes} mins</Text>
+                            </View>
+                        </View>
+                        <View style={styles.infoContainer}>
+                            <Text style={styles.infoHeader}>Ingredients</Text>
+                            <View style={styles.infoContent}>
+                                {
+                                    recipeDataParsed.extendedIngredients.map((ingredient) => {
+                                        return (
+                                            <View key={ingredient.id} style={{flexDirection: 'row'}}>
+                                                <Text style={styles.listItemIndicator}>-</Text>
+                                                <Text style={styles.listItem}>{ingredient.originalString}</Text>
+                                            </View>
+                                        );
+                                    })
+                                }
+                            </View>
+                        </View>
+                        <View style={styles.infoContainer}>
+                            <Text style={styles.infoHeader}>Instructions</Text>
+                            <View style={styles.infoContent}>
+                                {
+                                    recipeDataParsed.analyzedInstructions[0].steps.map((step) => {
+                                        return (
+                                            <View key={step.number} style={{flexDirection: 'row'}}>
+                                                <Text style={styles.listItemIndicator}>{step.number}. </Text>
+                                                <Text style={styles.listItem}>{step.step}</Text>
+                                            </View>
+                                        );
+                                    })
+                                }
+                            </View>
+                        </View>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+        );
+    }
 
-    return (
-        <SafeAreaView style={styles.safeAreaView}>
-            <ScrollView contentContainerStyle={styles.scrollViewContent}>
-
-                <View style={styles.headerContainer}>
-                    <View style={styles.nameContainer}>
-                        <Text style={styles.recipeNameText}>{recipeData.title}</Text>
-                    </View>
-                    <View>
-                        <Thumbnail style={styles.thumbnail} source={{uri: recipeData.image}}/>
-                    </View>
-                </View>
-
-                <View style={styles.bodyContainer}>
-                    <View style={styles.infoContainer}>
-                        <Text style={styles.infoHeader}>Description</Text>
-                        <View style={styles.infoContent}>
-                            <Text style={styles.contentItem}>{recipeData.summary.replace(/(<([^>]+)>)/gi, "")}</Text>
-                        </View>
-                    </View>
-                    <View style={styles.infoContainer}>
-                        <Text style={styles.infoHeader}>Time</Text>
-                        <View style={styles.infoContent}>
-                            <Text style={styles.contentItem}>Preparation: {recipeData.preparationMinutes} mins</Text>
-                            <Text style={styles.contentItem}>Cooking: {recipeData.cookingMinutes} mins</Text>
-                        </View>
-                    </View>
-                    <View style={styles.infoContainer}>
-                        <Text style={styles.infoHeader}>Ingredients</Text>
-                        <View style={styles.infoContent}>
-                            {
-                                recipeData.extendedIngredients.map((ingredient) => {
-                                    return (
-                                        <View key={ingredient.id} style={{flexDirection: 'row'}}>
-                                            <Text style={styles.listItemIndicator}>-</Text>
-                                            <Text style={styles.listItem}>{ingredient.originalString}</Text>
-                                        </View>
-                                    );
-                                })
-                            }
-                        </View>
-                    </View>
-                    <View style={styles.infoContainer}>
-                        <Text style={styles.infoHeader}>Instructions</Text>
-                        <View style={styles.infoContent}>
-                            {
-                                recipeData.analyzedInstructions[0].steps.map((step) => {
-                                    return (
-                                        <View key={step.number} style={{flexDirection: 'row'}}>
-                                            <Text style={styles.listItemIndicator}>{step.number}. </Text>
-                                            <Text style={styles.listItem}>{step.step}</Text>
-                                        </View>
-                                    );
-                                })
-                            }
-                        </View>
-                    </View>
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    );
+    else {
+        return (
+            <View>
+                <Text>Generating Recipe...</Text>
+            </View>
+        )
+    }
 
 }
